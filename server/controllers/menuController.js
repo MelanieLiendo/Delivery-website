ed919f48c9cae4d9848c1ee6d61408708bf21e1d
Melanie Liendo <meliliendo@hotmail.com>
2023-04-17 11:44:04 +0200

    menu add

--- server/controllers/menuController.js
+++ server/controllers/menuController.js
@@ -5,12 +5,16 @@ const Restaurant = require('../models/restaurant')
 const addMenu = async (req,res)=>{
     let {name, description, price, picture, category, restaurant}= req.body
     const findRestaurant = await Restaurant.findOne({restaurant})
+    const sku= `${name + findRestaurant._id.toString()}`
+    const findSku = await Menu.findOne({sku})
     try{
-        if (findRestaurant){
-            await Menu.create({restaurant_id: new ObjectId(findRestaurant._id), name, description, price, picture, category});
-            res.send({ok:true, data:"The menu was successfully added"})
-        }
-        else{
+        if (findRestaurant && !findSku){
+            // One step more validation- if in Menu we have already dish with certain name and restaurant id- we cannot create this record again
+            await Menu.create({restaurant_id: new ObjectId(findRestaurant._id), name, description, price, picture, category, sku});
+            res.send({ok:true, data:"The dish was successfully added"})
+        } else if (findRestaurant && findSku){
+            res.send({ok:true, data:"The dish already exists in this restaurant"})
+        } else {
             res.send({ok:true, data:"The restaurant doesn´t exist"})
         }
     }
--- server/models/menu.js
+++ server/models/menu.js
@@ -1,13 +1,22 @@
 const mongoose = require("mongoose");
-const ObjectId= require ('mongoose').Types.ObjectId
+//const ObjectId= require ('mongoose').Types.ObjectId
 
 const menuSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true},
   description: { type: String, required: true },
   price: { type: Number, required: true },
   picture: {type:String, required:true},
-  category: [{ type: String, required: true, unique: false }], /*promos,starters,main dishes,desserts,beverages*/
-  restaurant_id: {type : String, required: true}
+  category: [{ type: String, required: true, unique: false }],
+  sku: {type: String, required: true, unique: true}, /*promos,starters,main dishes,desserts,beverages*/
+ // restaurant_id: {type : String, required: true}
+  restaurant_id:{
+  type: mongoose.Schema.Types.ObjectId,
+  required:true,
+  ref:'restaurant'
+}
 });
 
 module.exports = mongoose.model("menu", menuSchema);
+
+
+
