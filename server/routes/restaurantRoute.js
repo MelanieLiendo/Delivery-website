39f02f41b16b6e4d05efc389625531bff6169981
Julieta Barrios <julietabarriosd@gmail.com>
2023-04-17 16:23:21 +0200

    register restaurant fixed

--- server/routes/restaurantRoute.js
+++ server/routes/restaurantRoute.js
@@ -2,7 +2,6 @@ const express = require ('express')
 router        = express.Router(),
     controller    = require('../controllers/restaurantController');
 
-router.post('/register', controller.registerRestaurant)
 router.post('/remove', controller.removeRestaurant)
 router.post('/update', controller.updateRestaurant)
 
