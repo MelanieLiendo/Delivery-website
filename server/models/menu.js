618e42799bbee7c96f2ce0a144139ef1a1727070
Julieta Barrios <julietabarriosd@gmail.com>
2023-04-17 10:43:30 +0200

    menu model changes

--- server/models/menu.js
+++ server/models/menu.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
+const ObjectId= require ('mongoose').Types.ObjectId
 
 const menuSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  picture: {type:Image, required:true},
+  picture: {type:String, required:true},
   category: [{ type: String, required: true, unique: false }], /*promos,starters,main dishes,desserts,beverages*/
   restaurant_id:{
-    type:Schema.Types.ObjectId,
+    type:restaurantSchema.ObjectId,
     required:true,
     ref:'restaurant'
 }
 });
 
-module.exports = mongoose.model("menu", productSchema);
+module.exports = mongoose.model("menu", menuSchema);
