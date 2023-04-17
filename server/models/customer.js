c4af87b1061202b98d86e7b99b02869a705c905a
Melanie Liendo <meliliendo@hotmail.com>
2023-04-17 14:09:02 +0200

    update

--- server/models/customer.js
+++ server/models/customer.js
@@ -5,6 +5,7 @@ const customerSchema = new mongoose.Schema({
   email:{type: String, required: true, unique: true},
   password:{type: String, required: true},
   address:{type: String, required: true}, /*maybe we need to add a new model with the addresses because then you can add new ones*/
+  admin: {type:Boolean, required:true, default: false}
 });
 
 module.exports = mongoose.model("customer", customerSchema);
