84bb707f619bd9918b782ed83ff046933ca1fbcc
Julieta Barrios <julietabarriosd@gmail.com>
2023-04-26 11:10:40 +0200

    starting with css

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -2,13 +2,16 @@ import React from 'react'
 import HomeCust from './Customers/HomeCust'
 import HomeRest from './Restaurant/HomeRest'
 import { NavLink } from 'react-router-dom'
+import homeImage from '../images/homeImage1.jpg'
 
 function Home({isLoggedIn,user}) {
     return (
-        <div>
+        <div className='home'>
         { isLoggedIn ? 
-       user.userType === "restaurant" ? <HomeRest user={user} /> : < HomeCust  user={user}/>: 
+       user.userType === "restaurant" ? <section ><HomeRest user={user} /> </section > : <section >< HomeCust  user={user}/></section>: 
        <>
+       <img src={homeImage}/>
+       <h1></h1>
        <h2>If you are a restaurant..</h2>
        <NavLink to="/register/restaurant">Register as a Restaurant</NavLink>
       <NavLink to="/login/restaurant">Login as a Restaurant</NavLink>
